126c181994cf14976cee619d83d1048c349670ff
Mark <mark@focorunner.com>
2018-01-27 13:49:21 -0700

    Add form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,17 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  
+  $('#messages').append(li);
 });
 
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
 
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('input[name=message]').val()
+  }, function() {});
+});
