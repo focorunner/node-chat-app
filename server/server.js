f742a19caec1ce32af54aff0ef3bea553ee47530
Mark <mark@focorunner.com>
2018-01-27 11:00:17 -0700

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port,() => {
+  console.log(`Started at port ${port}`);
+});
