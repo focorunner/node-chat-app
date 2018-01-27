fd98b98dc82f11862a8ed32a90d492beea2359c1
Mark <mark@focorunner.com>
2018-01-27 15:52:30 -0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
 });
 
 socket.on('disconnect', function() {
@@ -30,24 +29,33 @@ socket.on('newLocationMessage', function(message) {
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  var messageTextbox = $('input[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: $('input[name=message]').val()
-  }, function() {});
+    text: messageTextbox.val()
+  }, function() {
+    messageTextbox.val('');
+  });
 });
 
 var locationButton = $('#send-location');
 locationButton.on('click', function() {
   if(!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
-  } 
+  }
+
+  var buttonText = $('#send-location').text();
+  locationButton.attr('disabled', true).text('Sending Location...');
 
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text(buttonText);
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled').text(buttonText);
     alert('Unable to fetch location');
   });
 });
